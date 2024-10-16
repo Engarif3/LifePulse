7f2ad44f6276c932fc876371a8039251f22a436c
Md Arifur Rahman <arif.aust.eng@gmail.com>
2024-10-16 13:42:47 +0200

    search field applied to getAllAdmin

--- LifePulse_backend/src/app/modules/Admin/admin.controller.ts
+++ LifePulse_backend/src/app/modules/Admin/admin.controller.ts
@@ -2,12 +2,20 @@ import { Request, Response } from "express";
 import { adminService } from "./admin.service";
 
 const getAllAdmin = async (req: Request, res: Response) => {
-  const result = await adminService.getAllAdminFromDB();
-  res.status(200).json({
-    success: true,
-    message: "Admin info fetched",
-    data: result,
-  });
+  try {
+    const result = await adminService.getAllAdminFromDB(req.query);
+    res.status(200).json({
+      success: true,
+      message: "Admin info fetched",
+      data: result,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error?.name || "Something went wrong",
+      error: error,
+    });
+  }
 };
 
 export const adminController = {
--- LifePulse_backend/src/app/modules/Admin/admin.service.ts
+++ LifePulse_backend/src/app/modules/Admin/admin.service.ts
@@ -1,8 +1,25 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
-const getAllAdminFromDB = async () => {
-  const result = await prisma.admin.findMany();
+const getAllAdminFromDB = async (params: any) => {
+  const andConditions: Prisma.AdminWhereInput[] = [];
+  const searchingFields = ["name", "email"];
+
+  if (params.searchTerm) {
+    andConditions.push({
+      OR: searchingFields.map((field) => ({
+        [field]: {
+          contains: params.searchTerm,
+          mode: "insensitive",
+        },
+      })),
+    });
+  }
+
+  const whereConditions: Prisma.AdminWhereInput = { AND: andConditions };
+  const result = await prisma.admin.findMany({
+    where: whereConditions,
+  });
   return result;
 };
 
