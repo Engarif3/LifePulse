944076c9f565edd9183fc08cf65b8195a880279b
Md Arifur Rahman <arif.aust.eng@gmail.com>
2024-10-16 12:46:09 +0200

    user rsponse formatted

--- LifePulse_backend/src/app/modules/User/user.controller.ts
+++ LifePulse_backend/src/app/modules/User/user.controller.ts
@@ -2,8 +2,20 @@ import { Request, response, Response } from "express";
 import { userService } from "./user.service";
 
 const createAdmin = async (req: Request, res: Response) => {
-  const result = await userService.createAdminToDB(req.body);
-  res.send(result);
+  try {
+    const result = await userService.createAdminToDB(req.body);
+    res.status(200).json({
+      success: true,
+      message: "Admin created successfully !!!",
+      data: result,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error?.name || "Something went wrong",
+      error: error,
+    });
+  }
 };
 
 export const userController = {
